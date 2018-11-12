4408a8580dc52c4d978518741fc289c8192fc091
Артём <artem@artems-macbook-pro.local>
2018-11-12 20:41:52 +0300

    Initial Commit

---
+++ FILE_SYSTEM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FILE_SYSTEM
+//
+//  Created by Артём on 12/11/2018.
+//  Copyright © 2018 Ерошев Артём. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
